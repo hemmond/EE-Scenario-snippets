9981f14daf0f01515510183234712f223b0ef15c
Tomáš "hemmond" Látal <gitlab@hemmond.cz>
2021-04-25 17:45:25 +0200

    Bugfixes and added GM Menu inside module.

--- scenario_00_tractor_beam.lua
+++ scenario_00_tractor_beam.lua
@@ -6,17 +6,7 @@
 require("utils_tractorBeam.lua")
 --require("utils_tractorBeam_wrap.lua")
 
-function init()
-    --tractorBeam_init()
-    
-    SpaceStation():setPosition(1000, 1000):setTemplate('Small Station'):setFaction("Human Navy"):setCallSign("Small station"):setRotation(random(0, 360))
-    Artifact():setPosition(1000, 5000):setModel("small_frigate_1"):setDescription("Artifact")
-    Asteroid():setPosition(2000, 5000):setSize(random(100, 500))
-    VisualAsteroid():setPosition(3000, 5000):setSize(random(100, 500))
-    
-    local player = PlayerSpaceship():setPosition(500, 5000):setFaction("Human Navy"):setTemplate("Atlantis"):setRotation(200)
-    tractorBeam:enable(player)
-    
+function GM_buttons_init()
     addGMFunction(
         "Tractor ON",
         function()
@@ -41,13 +31,23 @@ function init()
         end
     )
     
+    tractorBeam:GMButtons(GM_buttons_init)
+end 
+
+function init()
+    --tractorBeam_init()
+    
+    SpaceStation():setPosition(1000, 1000):setTemplate('Small Station'):setFaction("Human Navy"):setCallSign("Small station"):setRotation(random(0, 360))
+    Artifact():setPosition(1000, 5000):setModel("small_frigate_1"):setDescription("Artifact")
+    Asteroid():setPosition(2000, 5000):setSize(random(100, 500))
+    VisualAsteroid():setPosition(3000, 5000):setSize(random(100, 500))
+    
+    local player = PlayerSpaceship():setPosition(500, 5000):setFaction("Human Navy"):setTemplate("Atlantis"):setRotation(200)
+    tractorBeam:enable(player)
+    
+    GM_buttons_init()
 end
 
 function update(delta)
-    for pidx=1,32 do
-        local player_ship = getPlayerShip(pidx)
-		if player_ship ~= nil and player_ship:isValid() then
-            tractorBeam:update(player_ship)
-        end
-    end
+    tractorBeam:update(delta)
 end
